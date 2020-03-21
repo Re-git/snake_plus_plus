d9a7fe836d083f7495db6dded0c40d2eda6e7499
Re-git <17963123+vejitatoja@users.noreply.github.com>
2020-03-21 23:53:44 +0100

    fixed angel bug

--- src/snake.cpp
+++ src/snake.cpp
@@ -11,7 +11,7 @@ Snake::Snake(int length)
     position.x = GetScreenWidth() / static_cast<float>(2.0);
     position.y = GetScreenHeight() / static_cast<float>(2.0);
     velocity = Vector2{3,0};
-    maxSpeed = 5;
+    maxSpeed = 8;
     turnRate = 0.2;
     angle=0;
     for (int i = 0; i < length; ++i)
@@ -27,6 +27,15 @@ void Snake::update()
     tail.erase(tail.end() - 1);
     move();
     checkCollisionWithEdges(position);
+    if (angle > 2*PI)
+    {
+        angle = angle - 2*PI;
+    }
+    if (angle < -2*PI)
+    {
+        angle = angle + 2*PI;
+    }
+    
     limit(velocity, maxSpeed);
 }
 
@@ -34,10 +43,10 @@ void Snake::update()
 void Snake::draw()
 {
     // Rysujemy snake'a
-    for (std::size_t i = 1; i < tail.size()-1; ++i)
+    for (std::size_t i = 0; i < tail.size(); ++i)
     {
-        //DrawRectangleRec({tail[i].x, tail[i].y, bodyWidth, bodyHeight},GREEN);
-        DrawLineBezier(Vector2{tail[i].x,tail[i].y},Vector2{tail[i+1].x,tail[i+1].y},10,DARKGREEN);
+        DrawRectangleRec({tail[i].x, tail[i].y, bodyWidth, bodyHeight},RED);
+        DrawLineBezier(Vector2{tail[i].x,tail[i].y},Vector2{tail[i+1].x,tail[i+1].y},2,DARKGREEN);
     }
         DrawTexturePro(sprite, {0.0f, 0.0f, (float)sprite.width, (float)sprite.height},
                                      {position.x, position.y, headWidth, headHeight},
@@ -60,7 +69,7 @@ bool Snake::checkCollisionWithEdges(Vector2 &pos)
     int height = GetScreenHeight();
     bool collide = false;
     std::cout << "SNAKE ANGLE:" << angle << std::endl;
-    if (pos.x > width-8)  {pos.x = width-8;  if(angle>PI) angle=angle-PI+2*angle; else angle=PI-angle;  collide=true;}
+    if (pos.x > width-8)  {pos.x = width-8;  if(angle>PI) angle=angle-PI-2*angle; else angle=PI-angle;  collide=true;}
     if (pos.x < 4)        {pos.x = 4;        if(angle>PI) angle=2*PI-(angle-PI); else angle=PI-angle;  collide=true;}
     if (pos.y > height-7) {pos.y = height-7;  if(angle>PI/2) angle=2*PI-angle; else angle=2*PI-angle;  collide=true;}
     if (pos.y < 60)        {pos.y = 60;        if(angle>1.5*PI) angle=angle-PI-(2*(angle-1.5*PI)); else angle=angle-PI+(2*(1.5*PI-angle));  collide=true;}
