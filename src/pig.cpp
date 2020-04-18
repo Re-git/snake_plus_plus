7572250e9d41fc3354b7bf2954585aba21853cf8
Bartosz Stańczyk <59917338+bartoszstanczyk@users.noreply.github.com>
2020-04-19 00:38:19 +0200

    świnia

---
+++ src/pig.cpp
@@ -0,0 +1,106 @@
+#include "pig.h"
+#include <iostream>
+
+Pig::Pig(Texture2D monkeySprite)
+{
+    sprite = monkeySprite;
+    position = createPosition();
+    velocity = Vector2{ 0,0 };
+    acceleration = Vector2{ 0,0 };
+    width = 50;
+    height = 50;           
+    maxspeed = 1.5;                       // max prêdkoœæ danej ma³pki
+    maxSeekForce = 0.055;               // si³a z jak¹ ma³pki zmieniaj¹ swoj¹ trajektorie ¿eby pod¹¿aæ za graczem
+    dead = 0;
+    frozen = 0;
+}
+
+void Pig::draw()
+{
+    monkeyRec = { position.x,position.y,width, height };
+    DrawTexturePro(sprite, { 0.0f, 0.0f, (float)sprite.width, (float)sprite.height },
+        monkeyRec, { 0.0f, 0.0f }, 0.0f, (frozen == 0) ? WHITE : SKYBLUE);
+}
+
+void Pig::update()
+{
+    // Update velocity
+    velocity = Vector2Add(velocity, acceleration);
+    // Limit speed
+    limit(velocity, maxspeed);
+    position = Vector2Add(position, velocity);
+    // Reset accelertion to 0 each cycle
+    acceleration = Vector2Scale(acceleration, 0);
+    draw();
+    maxspeed += 0.001;
+    if (separationRange > 60)
+    {
+        separationRange -= 0.1;
+    }
+}
+
+
+void Pig::applyForce(Vector2 force) {
+    // We could add mass here if we want A = F / M
+    acceleration = Vector2Add(acceleration, force);
+}
+
+void Pig::limit(Vector2& v, float num)
+{
+    if (v.x > num) { v.x = num; }
+    if (v.x < -num) { v.x = -num; }
+    if (v.y > num) { v.y = num; }
+    if (v.y < -num) { v.y = -num; }
+}
+
+void Pig::applyBehaviors(std::vector<Pig>& malpy, Vector2 owocPosition) {
+    Vector2 separateForce = separate(malpy);
+    Vector2 seekForce = seek(owocPosition);
+    separateForce = Vector2Scale(separateForce, 2);
+    seekForce = Vector2Scale(seekForce, 1);
+    applyForce(separateForce);
+    applyForce(seekForce);
+}
+
+// A method that calculates a steering force towards a target
+// STEER = DESIRED MINUS VELOCITY
+Vector2 Pig::seek(Vector2 target) {
+    Vector2 desired = Vector2Subtract(target, position);  // A vector pointing from the position to the target
+
+    // Normalize desired and scale to maximum speed
+    desired = Vector2Normalize(desired);
+    desired = Vector2Scale(desired, maxspeed);
+    // Steering = Desired minus velocity
+    Vector2 steer = Vector2Subtract(desired, velocity);
+    limit(steer, maxSeekForce);
+
+    return steer;
+
+Vector2 Pig::createPosition()
+{
+    Vector2 vec;
+    int roll = GetRandomValue(1, 4);
+    switch (roll)
+    {
+    case 1:
+        vec.x = -width;
+        vec.y = rand() % GetScreenHeight();
+        break;
+    case 2:
+        vec.x = rand() % GetScreenWidth();
+        vec.y = -height;
+        break;
+    case 3:
+        vec.x = GetScreenWidth() + width;
+        vec.y = rand() % GetScreenHeight();
+        break;
+    case 4:
+        vec.x = rand() % GetScreenWidth();
+        vec.y = GetScreenHeight() + height;
+        break;
+
+    default:
+        break;
+    }
+    return vec;
+}
