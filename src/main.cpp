df4ae585891052fab76803c525a5333670086abe
Kacper <you@example.com>
2020-05-07 13:39:15 +0200

    zjadanie w SPM

--- src/main.cpp
+++ src/main.cpp
@@ -78,7 +78,7 @@ Texture2D snekPacSprite = LoadTexture("assets/sprites/powerups/death.png");
 
   // LOAD SOUNDS
   InitAudioDevice();
-  Sound EatPM = LoadSound("assets/sounds/eatPM.wav");
+  Sound EatPM = LoadSound("assets/sounds/eatPac.wav");
   Sound Bomb = LoadSound("assets/sounds/Bomb.mp3");
   Sound frostBomb = LoadSound("assets/sounds/laser4.ogg");
   Sound bulletIn = LoadSound("assets/sounds/bullet_in.wav");
